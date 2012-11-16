564a7a0a482f706c976b819341d0c3511facd770
Jason Bryer <jason@bryer.org>
2012-11-16 09:31:29 -0500

    Default parameter is now a valid R statement. As such, constants (i.e. strings) must be quoted.

--- data/StudentsInRange.sql
+++ data/StudentsInRange.sql
@@ -1,9 +1,9 @@
 #' Students enrolled within the given date range.
 #' 
 #' @param startDate the start of the date range to return students.
-#' @default startDate 2011-07-01
+#' @default startDate '2012-07-01'
 #' @param endDate the end of the date range to return students.
-#' @default endDate 2012-08-01
+#' @default endDate toupper(format(Sys.Date()-14, '15-%b-%Y'))
 #' @return all students
 SELECT * 
 FROM students 
