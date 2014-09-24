9173402b44b290a03e4ca3d5f2b0036fed81c6ff
Jason Bryer <jason@bryer.org>
2014-09-24 12:13:35 -0400

    Added support for RJDBC.

--- R/sqlexec.R
+++ R/sqlexec.R
@@ -41,54 +41,37 @@ recodeColumns <- function(df, maxLevels=20) {
 sqlexec <- function(connection, sql, ...) { UseMethod("sqlexec") }
 
 #' Executes queries for RODBC package.
-#' 
-#' @param connection the database connection.
-#' @param sql the query to execute.
-#' @param ... other parameters passed to the appropriate \code{sqlexec} function.
-#' @return a data frame.
-#' @method sqlexec RODBC
-#' @export
+#' @inheritParams sqlexec
 sqlexec.RODBC <- function(connection, sql, ...) {
 	require(RODBC)
 	sqlQuery(connection, sql) #TODO: Why doesn't this work with ... passed through
 }
 
 #' Executes queries for RSQLite package.
-#' 
-#' @param connection the database connection.
-#' @param sql the query to execute.
-#' @param ... other parameters passed to the appropriate \code{sqlexec} function.
-#' @return a data frame.
-#' @method sqlexec SQLiteConnection
-#' @export
+#' @inheritParams sqlexec
 sqlexec.SQLiteConnection <- function(connection, sql, ...) {
 	require(RSQLite)
 	dbGetQuery(connection, sql, ...)
 }
 
 #' Executes queries for RMySQL package.
-#' 
-#' @param connection the database connection.
-#' @param sql the query to execute.
-#' @param ... other parameters passed to the appropriate \code{sqlexec} function.
-#' @return a data frame.
-#' @method sqlexec RMySQL
-#' @export
+#' @inheritParams sqlexec
 sqlexec.RMySQL <- function(connection, sql, ...) {
 	require(RMySQL)
 	dbSendQuery(connection, sql, ...)
 }
 
 #' Executes queries for RPostgreSQL
-#' 
-#' @param connection the database connection.
-#' @param sql the query to execute.
-#' @param ... other parameters passed to the appropriate \code{sqlexec} function.
-#' @return a data frame.
-#' @method sqlexec PostgreSQLConnection
-#' @export
+#' @inheritParams sqlexec
 sqlexec.PostgreSQLConnection <- function(connection, sql, ...) {
 	require(RPostgreSQL)
 	rs <- dbSendQuery(connection, sql)
 	fetch(rs, n=-1)
+}
+
+#' Executes queries for RJDBC
+#' @inheritParams sqlexec
+sqlexec.JDBCConnection <- function(connection, sql, ...) {
+	require(RJDBC)
+	RJDBC::dbGetQuery(connection, sql)
 }
